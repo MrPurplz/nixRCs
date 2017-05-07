e087af9d7dcb5bfb01f44fafecfa03fa2a95b860
mrpurplz <mrpurplz@users.noreply.github.com>
2017-05-07 16:25:03 +0100

    Initial Commit

---
+++ pullRC.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+gh="https://github.com/MrPurplz/nixRCs/"
+base="~/"
+declare -a rcfiles=(
+    '.vimrc'
+    '.tmux.conf'
+    '.bashrc'
+    )
+
+count=0
+while [ "x${rcfiles[count]}" != "x" ]
+do
+       cd
+       echo "$base${rcfiles[count]}"
+       if [ -f $base${rcfiles[count]} ]; then
+           echo "Found rc file. Backing up to $base${rcfiles[count]}.bak"
+           cp "$base${rcfiles[count]}" "$base${rcfiles[count]}.bak"
+       fi
+       echo "Pulling from Github: $gh${rcfiles[count]}"
+       #wget $gh${rcfiles[x]}
+       count=$(( $count + 1 ))
+done
+
